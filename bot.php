1a81c5566cc1b25c76f34cff36f9e712954523f5
Odd <odd@Orns-MacBook-Pro.local>
2016-10-12 23:04:47 +0700

    เพิ่ม curl request ผ่าน proxy server

--- bot.php
+++ bot.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:zH7bCPxiSXy2zy7';
+
 $access_token = 'Dj/fyxcVSFBdnf61vPwFQLZaXOj6YL9XZ68K0AJQeqMOAGkcKK1vPc8K3DsAdKvOVR5RxR91N+lRE/gJY0bFyK4NjQq9NAdYXrzLA0jSWWRk3nlKno0rcEe58yUmvtaZ7bG8PcMz7RUghxztTMXNqwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -37,6 +40,8 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 			$result = curl_exec($ch);
 			curl_close($ch);
 
--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'velodrome.usefixie.com:80';
+$proxyauth = 'fixie:zH7bCPxiSXy2zy7';
+
 $access_token = 'Dj/fyxcVSFBdnf61vPwFQLZaXOj6YL9XZ68K0AJQeqMOAGkcKK1vPc8K3DsAdKvOVR5RxR91N+lRE/gJY0bFyK4NjQq9NAdYXrzLA0jSWWRk3nlKno0rcEe58yUmvtaZ7bG8PcMz7RUghxztTMXNqwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +12,8 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
 
